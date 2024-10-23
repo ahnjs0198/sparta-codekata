ec2a1dbbb4f75133d0fc08f7986aeba37706f540
Jaesol Ahn <ahnjs0198@gmail.com>
2024-10-23 18:09:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,18 +1,19 @@
 class Solution {
     public int solution(int num) {
         int answer = 0;
+        long n = num;
         while (answer < 500){
-            if(num%2 == 0){
-                num /= 2;
+            if(n%2 == 0){
+                n /= 2;
                 answer++;
-            } else if (num == 1){
+            } else if (n == 1){
                 break;
             } else {
-                num = num*3 +1;
+                n = n*3 +1;
                 answer++;
             }
         }
-        if (answer >= 400){
+        if (answer >= 500){
             answer = -1;
         }
         return answer;
